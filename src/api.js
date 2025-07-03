822f25fb3f03c0ac934a6a3061fce171951e931f
dasuntheekshanadev <dasuntheekshana12@gmail.com>
2025-07-03 23:19:26 +0530

    new

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://13.219.89.22:5000/api';
+const API_URL = 'http://13.219.89.22:5000/';
 
 export const register = async (formData) => {
   const response = await axios.post(`${API_URL}/register`, formData, {
